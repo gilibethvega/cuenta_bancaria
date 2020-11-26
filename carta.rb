e8d035c9a61bb535dfa855e0036866c3db0ed359
Gilibeth Vega <gvega@creditu.com>
2020-11-25 23:11:54 -0300

    se corrije desafio 1

--- carta.rb
+++ carta.rb
@@ -6,10 +6,17 @@ class Carta
   end
 end
 
-num1 = Random.rand(1..13)
-pint1 = ['A', 'B', 'C', 'D'].sample(1)
-n1 = Carta.new(num1, pint1)
+n = 5
+array_card = []
+i = 0
 
-puts num1
-puts pint1
-puts n1
+while i < n
+  num1 = Random.rand(1..13)
+  pint1 = ['A', 'B', 'C', 'D'].sample(1)
+  n1 = Carta.new(num1, pint1)
+  array_card.push n1
+  i += 1
+end
+
+
+print array_card
